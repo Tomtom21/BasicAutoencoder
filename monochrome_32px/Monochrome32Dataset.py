e82ea5be17b2b53c743cef366053008329a75cb7
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2026-06-15 11:01:43 -0400

    New overriding class for the monochrome 32 dataset

--- monochrome_32px/Monochrome32Dataset.py
+++ monochrome_32px/Monochrome32Dataset.py
@@ -1,34 +1,10 @@
-import os
-import numpy as np
-import torch
-from PIL import Image
-from torch.utils.data import Dataset
-from tqdm import tqdm
+from ImageDataset import ImageDataset
 
-class Monochrome32Dataset(Dataset):
+class Monochrome32Dataset(ImageDataset):
     def __init__(self, data_dir_path):
-        self.data_dir_path = data_dir_path
-        self.image_files = sorted(
-            f for f in os.listdir(self.data_dir_path)
-            if f.endswith(".png") and not f.startswith(".")
+        super().__init__(
+            data_dir_path=data_dir_path,
+            color_mode="L",
+            target_size=(32,32),
+            normalize=True
         )
-
-        images = []
-        for image_file in tqdm(self.image_files, desc="Loading images"):
-            image_path = os.path.join(self.data_dir_path, image_file)
-            
-            # Loading the image as a grayscale image and normalizing to [0, 1]
-            img = Image.open(image_path).convert("L")
-            img_array = np.array(img, dtype=np.float32) / 255.0
-            
-            images.append(img_array)
-            img.close()
-
-        self.images = torch.from_numpy(np.stack(images, axis=0))
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        return self.images[idx]
-
